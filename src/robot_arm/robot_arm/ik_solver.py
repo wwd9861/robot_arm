250644f6e2d07b2e3b9e94d7ca7e70887ddf8ae1
wwd9861 <wwd9861@naver.com>
2025-03-03 01:14:07 +0900

    add ros2_control

--- src/robot_arm/robot_arm/ik_solver.py
+++ src/robot_arm/robot_arm/ik_solver.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 
-from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from robot_arm_msg.srv import Coordinate
 
 from math import *
@@ -17,10 +17,13 @@ class IKSolver(Node):
     def __init__(self):
         super().__init__("ik_solver")
         self.srv = self.create_service(Coordinate, "ik", self.ik_callback)
-        self.data = JointState()
-        self.data.name = ["revolute1", "revolute2", "revolute3", "revolute4"]
-        self.timer_period = 0.1
-        self.publisher = self.create_publisher(JointState, "/joint_states", 10)
+        self.data = JointTrajectory()
+        self.data.header.frame_id = "world"
+        self.data.joint_names = ["revolute1", "revolute2", "revolute3", "revolute4"]
+        self.points = JointTrajectoryPoint()
+        self.points.time_from_start.sec = 1
+
+        self.publisher = self.create_publisher(JointTrajectory, "/joint_trajectory_controller/joint_trajectory", 10)
 
     def ik_callback(self, request, response):
         theta1 = atan2(request.z, request.x)
@@ -34,9 +37,8 @@ class IKSolver(Node):
         theta2 = atan2(request.y - l1 - l4 * sin(request.phi), sqrt(request.x**2 + request.z**2) - l4 * cos(request.phi)) - atan2(k2, k1)
         theta4 = request.phi - theta2 - theta3
         theta1 = atan2(request.z, request.x)
-        joint_solution = [-theta1, theta2 - pi / 2, -theta3, theta4]
-        self.data.header.stamp = self.get_clock().now().to_msg()
-        self.data.position = joint_solution
+        self.points.positions = [-theta1, theta2 - pi / 2, -theta3, theta4]
+        self.data.points = [self.points]
         self.publisher.publish(self.data)
 
         return response
--- src/robot_arm_description/launch/robot_arm_launch.py
+++ src/robot_arm_description/launch/robot_arm_launch.py
@@ -1,7 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
@@ -35,4 +35,20 @@ def generate_launch_description():
         output="screen",
     )
 
-    return LaunchDescription([gazebo, node_robot_state_publisher, spawn_entity])
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=["ros2", "control", "load_controller", "--set-state", "active", "joint_state_broadcaster"], output="screen"
+    )
+
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=["ros2", "control", "load_controller", "--set-state", "active", "joint_trajectory_controller"], output="screen"
+    )
+
+    return LaunchDescription(
+        [
+            gazebo,
+            node_robot_state_publisher,
+            spawn_entity,
+            load_joint_state_broadcaster,
+            load_joint_trajectory_controller,
+        ]
+    )
